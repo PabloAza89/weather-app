2e4085f757375b0da28e37940de1ef4db0a63d4c
PabloAza89 <juanpabloazambuyo@gmail.com>
2022-08-08 02:11:43 -0300

    h

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,5 +2,30 @@ import React from 'react';
 
 export default function Card(props) {
   // acá va tu código
-  return <div>Card Component</div>
+  let weatherIcon = `http://openweathermap.org/img/wn/${props.img}@2x.png`
+  return <div>
+    Card Component
+    <table>
+      <tr>
+          <th>
+            {props.name}
+          </th>
+      </tr>
+      <tr>
+          <th>
+            Min
+            Max
+            <img className="wheather-photo" src={weatherIcon} alt={"weatherIcon"}/>
+          </th>
+      </tr>
+      <tr>
+          <th>
+          {Math.round((props.max-32)*(5/9))}º
+          {Math.round((props.min-32)*(5/9))}º
+          
+          </th>
+          
+      </tr>
+    </table>
+  </div>
 };
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,5 +3,14 @@ import React from 'react';
 export default function Cards(props) {
   // acá va tu código
   // tip, podés usar un map
-  return <div>Cards Component</div>
+  
+  //const numbers = props.cities[0];
+  const listItems = props.cities.map((number) =>
+    <li>{number}</li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );       
+  
+    
 };
