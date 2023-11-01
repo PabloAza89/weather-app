1996f75f802c8ad65caf6ba093c2d0ddf01589f3
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2023-11-01 20:26:20 -0300

    auto deploy works, but api key are not thruly hidden..

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -35,7 +35,7 @@ const NavBar = () => {
         style={s.linkAbout({ width, minPort, wrongPath, location:location.pathname })}
       >
         <Typography sx={s.typoAbout({ darkMode })}>
-          { english ? `ABOUT3` : `ACERCA` }
+          { english ? `ABOUT` : `ACERCA` }
         </Typography>
       </Link>
       <Box
